a6dbe82aa86c6616db84e4626ffc1d25d85926c0
Y.A. <younesabourrig@gmail.com>
2025-07-29 13:54:42 +0100

    js file just for the nav bare

---
+++ script.js
@@ -0,0 +1,19 @@
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+// get in the learn mor button
+// const lmp1btn = document.getElementById("lmp1btn");
+// const showedpr = document.getElementById("showedpr");
+
+// lmp1btn.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   showedpr.style.display = "block";
+// });
+
+// lmp1btn.addEventListener("blur", function (e) {
+//   e.preventDefault();
+//   showedpr.style.display = "none";
+// });
